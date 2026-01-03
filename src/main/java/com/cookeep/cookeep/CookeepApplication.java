84a5db5339debc8de12db6036cb2c74d52a6d46b
0-x-14 <0x14@daum.net>
2026-01-03 22:01:55 +0900

    chore: initial commit

---
+++ src/main/java/com/cookeep/cookeep/CookeepApplication.java
@@ -0,0 +1,13 @@
+package com.cookeep.cookeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookeep/cookeep/CookeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookeep.cookeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
