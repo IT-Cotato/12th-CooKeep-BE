9bc41190fccda989cf5897027b2765c9a6cd0cd1
hyunj24 <hyunjug7@naver.com>
2026-01-09 01:38:52 +0900

    feat: PlantService 추가

---
+++ src/main/java/com/cookeep/cookeep/domain/Plants/application/PlantService.java
@@ -0,0 +1,20 @@
+package com.cookeep.cookeep.domain.Plants.application;
+
+import com.cookeep.cookeep.domain.Plants.dao.PlantRepository;
+import com.cookeep.cookeep.domain.Plants.entity.Plants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PlantService {
+    private final PlantRepository plantRepository;
+
+    @Transactional(readOnly = true)
+    public List<Plants> getAllPlants() {
+        return plantRepository.findAll();
+    }
+}
