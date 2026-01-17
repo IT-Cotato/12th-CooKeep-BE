65601b76d1e5f690601a415ed224e8a9e7967dfe
hyunj24 <hyunjug7@naver.com>
2026-01-17 16:02:33 +0900

    fix: PLANT-005 에러 코드 앞 공백 삭제

--- src/main/java/com/cookeep/cookeep/common/exception/ErrorCode.java
+++ src/main/java/com/cookeep/cookeep/common/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
 	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘못되었습니다.", "COMMON-002"),
 	PLANT_NOT_FROZEN(HttpStatus.BAD_REQUEST, "성장 정지 상태가 아닙니다.", "PLANT-003"),
 	ALREADY_HARVESTED(HttpStatus.BAD_REQUEST, "이미 수확한 식물입니다.", "PLANT-004"),
-	PLANT_IS_FROZEN(HttpStatus.BAD_REQUEST, "성장 정지 상태의 식물입니다.", " PLANT-005"),
+	PLANT_IS_FROZEN(HttpStatus.BAD_REQUEST, "성장 정지 상태의 식물입니다.", "PLANT-005"),
 
 	// 403 FORBIDDEN (권한 관련)
 	NOT_MY_PLANT(HttpStatus.FORBIDDEN, "해당 식물에 대한 권한이 없습니다.", "PLANT-001"),
