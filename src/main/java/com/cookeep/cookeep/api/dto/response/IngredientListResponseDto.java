9be9a9127b19c4c36c21604b8a598f995b0442b0
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2026-02-07 22:16:08 +0900

    feat(ingredient): dto, repository 수정

---
+++ src/main/java/com/cookeep/cookeep/api/dto/response/IngredientListResponseDto.java
@@ -0,0 +1,46 @@
+package com.cookeep.cookeep.api.dto.response;
+
+import com.cookeep.cookeep.domain.ingredient.common.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class IngredientListResponseDto {
+
+    private List<CategoryGroup> categories;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class CategoryGroup {
+
+        private Category category;
+        private String displayName;
+        private List<IngredientItem> ingredients;
+
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class IngredientItem {
+
+        private Long id;
+
+        private String type;
+
+        private String name;
+
+        private String imageUrl;
+
+        private Category category;
+
+    }
+
+}
+
--- src/main/java/com/cookeep/cookeep/domain/ingredient/defaultingredient/dao/DefaultIngredientRepository.java
+++ src/main/java/com/cookeep/cookeep/domain/ingredient/defaultingredient/dao/DefaultIngredientRepository.java
@@ -1,7 +1,16 @@
 package com.cookeep.cookeep.domain.ingredient.defaultingredient.dao;
 
+import com.cookeep.cookeep.domain.ingredient.common.Category;
 import com.cookeep.cookeep.domain.ingredient.defaultingredient.entity.DefaultIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DefaultIngredientRepository extends JpaRepository<DefaultIngredient, Long> {
+
+    // 카테고리 별 기본 식재료 조회 (ID 오름차순으로 정렬)
+    List<DefaultIngredient> findByCategoryOrderByIdAsc(Category category);
+
+    // 모든 기본 식재료 조회 (ID 오름차순)
+    List<DefaultIngredient> findAllByOrderByIdAsc();
 }
