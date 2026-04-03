4d011215d7444cb610ee660e024a26ad9379fba5
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2026-04-03 23:54:27 +0900

    feat: 유통기한 관련 enum 추가

---
+++ src/main/java/com/cookeep/cookeep/domain/notification/entity/NotificationType.java
@@ -0,0 +1,38 @@
+package com.cookeep.cookeep.domain.notification.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum NotificationType {
+    EXPIRATION("유통기한임박"),
+    PLANT_WILTING("시듦"),
+    PLANT_GROWTH_STOP("성장정지");
+
+    private final String displayName;
+
+    @JsonCreator
+    public static NotificationType from(String value) {
+        for (NotificationType type : NotificationType.values()) {
+
+            // enum 이름으로 매핑 (EXPIRATION 등)
+            if (type.name().equalsIgnoreCase(value)) {
+                return type;
+            }
+
+            // displayName으로 매핑 (유통기한임박 등)
+            if (type.displayName.equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid notification type: " + value);
+    }
+
+    @JsonValue
+    public String toJson() {
+        return this.displayName;
+    }
+}
