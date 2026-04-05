6da211c1182d8a33c937712d35bb6bb66d582685
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2026-04-05 19:47:21 +0900

    fix: edit payload url

--- src/main/java/com/cookeep/cookeep/domain/notification/entity/NotificationType.java
+++ src/main/java/com/cookeep/cookeep/domain/notification/entity/NotificationType.java
@@ -12,7 +12,7 @@ public enum NotificationType {
             "유통기한임박",
             "유통기한임박",
             "오늘 유통기한이 만료되는 재료가 있어요! \n 지금 확인하고 요리해볼까요?",
-            "/api/users/me/web/push/notifications"),
+            "/api/users/me/refrigerator/home"),
 
     //필요시수정(to.현정)
     PLANT_WILTING(
