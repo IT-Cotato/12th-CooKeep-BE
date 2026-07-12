4803716589717ee6a0806a9e00b8a0bb0a5e3fa3
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2026-07-12 11:23:51 +0900

    COOKEEP-61 profile_image_id 컬럼에 not null 제약 추가

--- src/main/resources/db/migration/V9__add_profile_image_id_to_users.sql
+++ src/main/resources/db/migration/V9__add_profile_image_id_to_users.sql
@@ -1,4 +1,4 @@
 -- src/main/resources/db/migration/V9__add_profile_image_id_to_users.sql
 
 ALTER TABLE users
-    ADD COLUMN profile_image_id INT NULL DEFAULT 1;
+    ADD COLUMN profile_image_id INT NOT NULL DEFAULT 1;
