5a1ddf5ee05ede2127754709b914ac7111bd5985
0-x-14 <0_x_14@naver.com>
2026-07-22 10:47:10 +0900

    fix: 비밀번호 재설정 및 변경 시 특수문자 허용

--- src/main/java/com/cookeep/cookeep/api/dto/request/ResetPasswordRequestDTO.java
+++ src/main/java/com/cookeep/cookeep/api/dto/request/ResetPasswordRequestDTO.java
@@ -1,10 +1,10 @@
 package com.cookeep.cookeep.api.dto.request;
 
 import com.cookeep.cookeep.api.dto.validator.PasswordMatch;
+import com.cookeep.cookeep.api.dto.validator.ValidPassword;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 @PasswordMatch
 public record ResetPasswordRequestDTO(
@@ -14,10 +14,7 @@ public record ResetPasswordRequestDTO(
 
 	// 영문, 숫자 포함 8자 이상 값
 	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-	@Pattern(
-		regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
-		message = "영문, 숫자 포함 8자 이상의 비밀번호를 사용해주세요"
-	)
+	@ValidPassword
 	String password,
 
 	@NotBlank(message = "비밀번호 확인은 필수 입력 값입니다.")
